1a2eec4a43020d959865c96b064c9f4ba0788cfc
Jarred Kenny <jarred.kenny@tracktile.io>
2023-08-23 17:06:14 -0300

    chore: also reexport TypeSystem

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,6 +2,7 @@ export { Type as T, TypeGuard } from "@sinclair/typebox";
 export type { TSchema, Static } from "@sinclair/typebox";
 export { Value } from "@sinclair/typebox/value";
 export { TypeCompiler } from "@sinclair/typebox/compiler";
+export { TypeSystem } from "@sinclair/typebox/system";
 
 export * from "./model";
 export * from "./procedure";
